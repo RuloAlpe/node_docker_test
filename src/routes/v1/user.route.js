5c0ef171b7e45aa8da6cadb96769856e5c7af673
raul <raul.alanis.gin@gmail.com>
2021-09-29 21:35:29 -0500

    Initial setup

---
+++ src/routes/v1/user.route.js
@@ -0,0 +1,21 @@
+// import express from 'express';
+// import { getUsers, getUserById } from '../../controller/user';
+// const router = express.Router();
+
+// router.get('/', async (req, res) => {
+//   const userList = await getUsers(
+//     'https://jsonplaceholder.typicode.com/users/',
+//   );
+//   res.status(200).json(userList);
+// });
+
+// router.get('/:userId', async (req, res) => {
+//   const userIdToFind = req.params.userId;
+//   const userResult = await getUserById(
+//     'https://jsonplaceholder.typicode.com/users/',
+//     userIdToFind,
+//   );
+//   res.status(200).json(userResult);
+// });
+
+// export default router;
